06d936aa0af6ad33e479b8597039fa02d5ddbf9a
Daniel Macák <daniel.macak@email.cz>
2019-12-05 20:43:00 +0100

    Responsive and resizable

--- projects/demo/src/app/app.component.ts
+++ projects/demo/src/app/app.component.ts
@@ -14,7 +14,10 @@ export class AppComponent implements OnInit {
       'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fjackhaig.files.wordpress.com%2F2012%2F09%2Fimag0408.jpg&f=1&nofb=1',
       'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.fklm-tours.com%2Fwp-content%2Fuploads%2F2017%2F11%2FLandscapes-at-the-Semien-Mountains-1.jpg&f=1&nofb=1',
       'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2F1.bp.blogspot.com%2F-uUwHOcti87o%2FUQfYd3nDGrI%2FAAAAAAAAhOo%2FGpWmN3lX8pM%2Fs1600%2F864271721_3b663f4dc7_b.jpg&f=1&nofb=1',
-      'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.werfenweng.eu%2F_thumbnail_%2F436_11_Hochgebirgsrunde.jpg&f=1&nofb=1'
+      'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.werfenweng.eu%2F_thumbnail_%2F436_11_Hochgebirgsrunde.jpg&f=1&nofb=1',
+      'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.jE8bEuIvb0jhV1jPYBOwjgHaFj%26pid%3DApi&f=1',
+      'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmedia2.s-nbcnews.com%2Fj%2Fnewscms%2F2017_23%2F2027541%2F170606-lucuma-fruit-ew-238p_7fe0368ac968a5fd2ef60eab1398c2d6.nbcnews-ux-2880-1000.jpg&f=1&nofb=1',
+      'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.y-cFijq0bPXVSnnl88XKmQHaE7%26pid%3DApi&f=1'
     ];
   }
 }
--- projects/ngx-galerie/src/lib/ngx-galerie.component.ts
+++ projects/ngx-galerie/src/lib/ngx-galerie.component.ts
@@ -7,34 +7,29 @@ import {
   HostBinding,
   ViewChild,
   ElementRef,
-  Renderer2
+  Renderer2,
+  AfterViewInit,
+  OnDestroy
 } from '@angular/core';
+import { fromEvent, Subscription } from 'rxjs';
 
 @Component({
   selector: 'ngx-galerie',
   templateUrl: './ngx-galerie.component.html',
   styleUrls: ['./ngx-galerie.component.scss']
 })
-export class NgxGalerieComponent implements OnChanges, OnInit {
+export class NgxGalerieComponent implements OnChanges, OnInit, OnDestroy {
   @Input()
   items: string[];
-
-  @Input()
-  @HostBinding('style.width')
-  width = '500px';
-
-  @Input()
-  @HostBinding('style.height')
-  height = '500px';
 
   @Input()
   thumbsOrientation: 'top' | 'bottom' | 'left' | 'right' = 'left';
 
   @Input()
-  thumbWidth = '120px';
+  thumbWidth = 120;
 
   @Input()
-  thumbHeight = '80px';
+  thumbHeight = 80;
 
   @ViewChild('imageList', { static: false })
   imageList: ElementRef;
@@ -51,11 +46,12 @@ export class NgxGalerieComponent implements OnChanges, OnInit {
 
   // TODO rework selection mechanism
   selectedItem: string;
+  selectedItemX: number;
   itemWidth: number;
 
-  private currentImageListPosition = 0;
+  private resizeSub: Subscription;
 
-  constructor(private renderer: Renderer2) {}
+  constructor(private renderer: Renderer2, private elRef: ElementRef) {}
 
   ngOnChanges({ items }: SimpleChanges) {
     if (items.previousValue !== items.currentValue) {
@@ -63,53 +59,88 @@ export class NgxGalerieComponent implements OnChanges, OnInit {
     }
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    if (typeof window !== 'undefined') {
+      this.resizeSub = fromEvent(window, 'resize').subscribe(this.onResize);
+    }
+  }
 
-  onPanStart() {}
+  ngOnDestroy() {
+    this.resizeSub && this.resizeSub.unsubscribe();
+  }
+
+  onPanStart() {
+    this.selectedItemX = this.getTranslateX(this.imageList.nativeElement);
+  }
 
   onPan(e) {
     const imageList = this.imageList.nativeElement as HTMLUListElement;
 
     this.renderer.setStyle(imageList, 'transition', `transform 0s`);
-    this.setTranslateX(e.deltaX);
+    this.setTranslateX(e.deltaX + this.selectedItemX, imageList);
   }
 
-  onPanEnd() {
+  onPanEnd(e) {
+    const { items } = this;
     const imageList = this.imageList.nativeElement as HTMLUListElement;
 
-    this.renderer.setStyle(imageList, 'transition', `transform 0.3s`);
-    this.setTranslateX(this.currentImageListPosition);
+    this.renderer.setStyle(imageList, 'transition', '');
+
+    if (Math.abs(e.deltaX) > 100) {
+      const nextItemIndexDelta = e.deltaX > 0 ? -1 : 1;
+      const nextItem =
+        items[items.indexOf(this.selectedItem) + nextItemIndexDelta];
+      this.selectItem(nextItem);
+    } else {
+      const oldItemIndex = items.indexOf(this.selectedItem);
+      const x = this.itemWidth ? -oldItemIndex * this.itemWidth : 0;
+      this.setTranslateX(x, imageList);
+    }
   }
+
+  onResize = () => {
+    this.positionSelectedItem();
+  };
 
   selectItem(item: string) {
-    if (!this.itemWidth) {
-      const imageList = this.imageList.nativeElement as HTMLUListElement;
-
-      this.itemWidth = imageList
-        .querySelector('li')
-        .getBoundingClientRect().width;
-    }
-
-    const newItemIndex = this.items.indexOf(item);
-
-    this.setTranslateX(this.itemWidth * newItemIndex * -1);
+    this.selectedItem = item;
+    this.positionSelectedItem();
   }
 
-  private getTranslateX() {
-    const imageList = this.imageList.nativeElement as HTMLUListElement;
+  private positionSelectedItem() {
+    this.extractItemWidth();
 
-    const match = imageList.style.transform.match(/translate3D\((\d+)/);
+    const selectedItemIndex = this.items.indexOf(this.selectedItem);
 
-    return match && match[0];
-  }
-
-  private setTranslateX(x: number) {
-    const imageList = this.imageList.nativeElement as HTMLUListElement;
-
-    this.renderer.setStyle(
-      imageList,
-      'transform',
-      `translate3D(${x}px, 0px, 0px)`
+    this.setTranslateX(
+      -selectedItemIndex * this.itemWidth,
+      this.imageList.nativeElement
     );
+  }
+
+  private extractItemWidth() {
+    const imageList = this.imageList.nativeElement as HTMLUListElement;
+
+    const { width, marginLeft, marginRight } = getComputedStyle(
+      imageList.querySelector('li')
+    );
+
+    this.itemWidth =
+      parseInt(width) + parseInt(marginLeft) + parseInt(marginRight);
+  }
+
+  private extractItemWidthIfNeeded() {
+    if (!this.itemWidth) {
+      this.extractItemWidth();
+    }
+  }
+
+  private getTranslateX(el: HTMLElement): number {
+    const match = el.style.transform.match(/translate3d\((-?\d+)/);
+    return match && +match[1];
+  }
+
+  private setTranslateX(x: number, el: HTMLElement) {
+    this.renderer.setStyle(el, 'transform', `translate3D(${x}px, 0px, 0px)`);
   }
 }
