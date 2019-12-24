43c548074c05e8741338435ec22ab20203fffbd3
Daniel Macak <daniel.macak@post.at>
2019-12-24 13:00:49 +0100

    Minor style improvements

--- projects/detail-view/src/lib/detail-view.module.ts
+++ projects/detail-view/src/lib/detail-view.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { DetailViewComponent } from './detail-view.component';
 import { OverlayModule } from '@angular/cdk/overlay';
+// TODO change to outer import import ... from 'gallery'
 import { GalleryModule } from 'projects/gallery/src/public-api';
 
 @NgModule({
