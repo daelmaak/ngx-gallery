543104f83f0214847fb6833c77425773ca147580
Daniel Macák <daniel.macak@email.cz>
2020-01-24 22:30:03 +0100

    native scroll snap basics

--- projects/demo/src/app/app.component.ts
+++ projects/demo/src/app/app.component.ts
@@ -14,6 +14,8 @@ export class AppComponent implements OnInit {
   images: GalleryItem[];
   galleryDetailRef: GalleryDetailRef;
 
+  debug = 'scrollBehavior' in document.documentElement.style;
+
   @ViewChild('detailImageTemplate2', { static: false })
   detailImageTemplate2: TemplateRef<any>;
 
--- projects/gallery/src/lib/components/image-viewer/image-viewer.component.ts
+++ projects/gallery/src/lib/components/image-viewer/image-viewer.component.ts
@@ -11,17 +11,11 @@ import {
   OnInit,
   Output,
   SimpleChanges,
-  TemplateRef
+  TemplateRef,
+  ViewChild
 } from '@angular/core';
-import { fromEvent, Subject, Subscription } from 'rxjs';
-import {
-  filter,
-  repeat,
-  skip,
-  switchMap,
-  take,
-  takeWhile
-} from 'rxjs/operators';
+import { fromEvent, Subject } from 'rxjs';
+import { takeUntil, debounceTime } from 'rxjs/operators';
 import { GalleryItem } from '../../core/gallery-item';
 import { ImageFit } from '../../core/image-fit';
 
@@ -64,16 +58,18 @@ export class ImageViewerComponent implements OnChanges, OnInit, OnDestroy {
   @Output()
   selection = new EventEmitter<number>();
 
+  @ViewChild('imageList', { static: true }) imageListEl: ElementRef<
+    HTMLElement
+  >;
+
   imageStyles = {
     backgroundSize: 'contain'
   };
-  imageListStyles: any;
   imagesShown = false;
   imagesTransition = false;
 
+  private destroy$ = new Subject();
   private itemWidth: number;
-  private imagesHammerSub: Subscription;
-  private resizeSub: Subscription;
 
   get showPrevArrow() {
     return this.arrows && (this.selectedItem > 0 || this.loop);
@@ -87,7 +83,7 @@ export class ImageViewerComponent implements OnChanges, OnInit, OnDestroy {
 
   constructor(
     private zone: NgZone,
-    private elRef: ElementRef,
+    private elRef: ElementRef<HTMLElement>,
     private cd: ChangeDetectorRef
   ) {}
 
@@ -100,8 +96,25 @@ export class ImageViewerComponent implements OnChanges, OnInit, OnDestroy {
   ngOnInit() {
     this.imageCounter === undefined && (this.imageCounter = true);
 
+    // TODO mitigate lack of smooth scroll-behavior in Safari
+
+    // determining selected items upon native scroll in the image list
+    fromEvent(this.imageListEl.nativeElement, 'scroll')
+      .pipe(debounceTime(20), takeUntil(this.destroy$))
+      .subscribe(_ => {
+        this.selectedItem = Math.floor(
+          this.imageListEl.nativeElement.scrollLeft / this.itemWidth
+        );
+        if (this.selectedItem < 0) {
+          this.selectedItem = 0;
+        }
+        this.cd.detectChanges();
+      });
+
     if (typeof window !== 'undefined') {
-      this.resizeSub = fromEvent(window, 'resize').subscribe(this.onResize);
+      fromEvent(window, 'resize')
+        .pipe(takeUntil(this.destroy$))
+        .subscribe(this.onResize);
     }
     requestAnimationFrame(() => {
       this.onResize();
@@ -114,76 +127,12 @@ export class ImageViewerComponent implements OnChanges, OnInit, OnDestroy {
       // immediately after component creation. This change will be picked up later.
       this.imagesTransition = true;
     });
-
-    const direction = Hammer.DIRECTION_HORIZONTAL;
-
-    if (typeof Hammer !== 'undefined') {
-      const hammer = new Hammer(this.elRef.nativeElement);
-      hammer.get('pan').set({ direction, threshold: 5 });
-
-      this.zone.runOutsideAngular(() => {
-        const hammerInput$ = new Subject<HammerInput>();
-        hammer.on('pan', (e: HammerInput) => hammerInput$.next(e));
-
-        // This solves problem with Hammerjs, where although direction HORIZONTAL set and user touch-scrolls vertically,
-        // Hammer still emits like 5 events which can shift images to side.
-        // This code takes second event, which already knows the direction, and based on it determines, whether to accept the following
-        // events - because the whole touch movement is horizontal, or ignore them - because it is vertical
-        // Once a final event comes (touch movement is complete), the stream is restarted
-        this.imagesHammerSub = hammerInput$
-          .pipe(
-            skip(1),
-            take(1),
-            // if horizontal, accept all events, otherwise take only the final event
-            switchMap(e =>
-              e.direction & Hammer.DIRECTION_HORIZONTAL ||
-              e.offsetDirection & Hammer.DIRECTION_HORIZONTAL
-                ? hammerInput$
-                : hammerInput$.pipe(filter(ev => ev.isFinal))
-            ),
-            // complete the stream once the final event occurs, but still emit it
-            takeWhile(e => !e.isFinal, true),
-            repeat()
-          )
-          .subscribe(e => {
-            this.onPan(e);
-            if (e.isFinal) {
-              this.onPanEnd(e);
-            }
-          });
-      });
-    }
   }
 
   ngOnDestroy() {
-    this.imagesHammerSub && this.imagesHammerSub.unsubscribe();
-    this.resizeSub && this.resizeSub.unsubscribe();
+    this.destroy$.next(null);
+    this.destroy$.complete();
   }
-
-  onPan = (e: HammerInput) => {
-    if (e.eventType & Hammer.INPUT_CANCEL) {
-      return;
-    }
-
-    this.imagesTransition = false;
-    this.shiftImages(e.deltaX + -this.selectedItem * this.itemWidth);
-  };
-
-  onPanEnd = (e: HammerInput) => {
-    this.imagesTransition = true;
-
-    if (e.eventType & Hammer.INPUT_CANCEL) {
-      this.center();
-    } else if (
-      Math.abs(e.deltaX) > Math.abs(this.itemWidth / 3) ||
-      Math.abs(e.velocityX) > 0.3
-    ) {
-      e.deltaX > 0 ? this.prev() : this.next();
-    } else {
-      // no item to be selected, center the current
-      this.center();
-    }
-  };
 
   prev() {
     this.select(this.selectedItem - 1);
@@ -216,13 +165,10 @@ export class ImageViewerComponent implements OnChanges, OnInit, OnDestroy {
   }
 
   private center() {
-    this.shiftImages(-this.selectedItem * this.itemWidth);
+    this.shiftImages(this.selectedItem * this.itemWidth);
   }
 
   private shiftImages(x: number) {
-    this.imageListStyles = {
-      transform: `translate3D(${x}px, 0px, 0px)`
-    };
-    this.cd.detectChanges();
+    this.elRef.nativeElement.querySelector('ul').scrollLeft = x;
   }
 }
