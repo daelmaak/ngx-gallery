d4d8da8c8ca94555cbca835eb94f9cf75ec9276e
Daniel Macák <daniel.macak@email.cz>
2020-02-14 15:01:07 +0100

    Demo git page

--- projects/demo/src/app/app.component.ts
+++ projects/demo/src/app/app.component.ts
@@ -32,38 +32,38 @@ export class AppComponent implements OnInit, AfterViewInit {
   ngOnInit() {
     this.images = [
       {
-        src: '/assets/mountains1.jpg',
-        thumbSrc: '/assets/mountains1-thumb.jpg'
+        src: './assets/mountains1.jpg',
+        thumbSrc: './assets/mountains1-thumb.jpg'
       },
       {
-        src: '/assets/mountains2.jpg',
-        thumbSrc: '/assets/mountains2-thumb.jpg'
+        src: './assets/mountains2.jpg',
+        thumbSrc: './assets/mountains2-thumb.jpg'
       },
       {
-        src: '/assets/mountains3.jpg',
-        thumbSrc: '/assets/mountains3-thumb.jpg'
+        src: './assets/mountains3.jpg',
+        thumbSrc: './assets/mountains3-thumb.jpg'
       },
       {
-        src: '/assets/mountains4.jpg',
-        thumbSrc: '/assets/mountains4-thumb.jpg'
+        src: './assets/mountains4.jpg',
+        thumbSrc: './assets/mountains4-thumb.jpg'
       },
       {
-        src: '/assets/fruit1.jpg'
+        src: './assets/fruit1.jpg'
       },
       {
-        src: '/assets/fruit2.jpg',
-        thumbSrc: '/assets/fruit2-thumb.jpg'
+        src: './assets/fruit2.jpg',
+        thumbSrc: './assets/fruit2-thumb.jpg'
       },
       {
-        src: '/assets/fruit3.jpg'
+        src: './assets/fruit3.jpg'
       },
       {
-        src: '/assets/vogel1.jpg',
-        thumbSrc: '/assets/vogel1-thumb.jpg'
+        src: './assets/vogel1.jpg',
+        thumbSrc: './assets/vogel1-thumb.jpg'
       },
       {
-        src: '/assets/landscape1.jpg',
-        thumbSrc: '/assets/landscape1-thumb.jpg'
+        src: './assets/landscape1.jpg',
+        thumbSrc: './assets/landscape1-thumb.jpg'
       }
     ];
   }
