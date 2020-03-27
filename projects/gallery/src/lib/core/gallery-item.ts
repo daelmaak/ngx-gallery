ab4969b7ec3b67cb690a2ffeab9ec2838492229b
Daniel Macák <daniel.macak@email.cz>
2020-03-27 23:51:32 +0100

    Comments for gallery items

--- projects/gallery/src/lib/core/gallery-item.ts
+++ projects/gallery/src/lib/core/gallery-item.ts
@@ -1,12 +1,39 @@
 export interface GalleryItem {
+  /**
+   * Media url
+   */
   src: string;
+
+  /**
+   * Url of media thumbnail
+   */
   thumbSrc?: string;
+
+  /**
+   * Alt text for not yet loaded image
+   */
   alt?: string;
+
+  /**
+   * Makes item's media be rendered as HTML video.
+   * This property doesn't mark youtube videos. Those don't need this flag as they are recongnized automatically
+   */
   video?: boolean;
+
+  /**
+   * Custom data where you can put whatever you want
+   */
   data?: any;
 }
 
 export interface GalleryItemInternal extends GalleryItem {
+  /**
+   * Marks item as loaded once its media gets loaded
+   */
   _loaded?: boolean;
+
+  /**
+   * Marks item as visited at least once
+   */
   _visited?: boolean;
 }
