acdb9f19e503353ab91bce3cf5180f9dc5d0bae2
Daniel Macák <daniel.macak@email.cz>
2020-04-02 06:56:53 +0200

    Custom error template

--- projects/gallery/src/lib/components/image-viewer/image-viewer.component.ts
+++ projects/gallery/src/lib/components/image-viewer/image-viewer.component.ts
@@ -45,16 +45,13 @@ export class ImageViewerComponent implements OnChanges, OnInit, OnDestroy {
   arrows: boolean;
 
   @Input()
-  prevArrowTemplate: TemplateRef<void>;
-
-  @Input()
-  nextArrowTemplate: TemplateRef<void>;
-
-  @Input()
   selectedIndex: number;
 
   @Input()
   descriptions: boolean;
+
+  @Input()
+  errorText: string;
 
   @Input()
   imageCounter: boolean;
@@ -76,6 +73,15 @@ export class ImageViewerComponent implements OnChanges, OnInit, OnDestroy {
 
   @Input()
   loadingTemplate: TemplateRef<void>;
+
+  @Input()
+  errorTemplate: TemplateRef<void>;
+
+  @Input()
+  prevArrowTemplate: TemplateRef<void>;
+
+  @Input()
+  nextArrowTemplate: TemplateRef<void>;
 
   @Input()
   galleryMainAxis: Orientation;
