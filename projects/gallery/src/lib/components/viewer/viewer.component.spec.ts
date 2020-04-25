5f056a1a184501fa2d86b29b68f0e799ae78da97
dann <daniel.macak@email.cz>
2020-04-25 20:15:58 +0200

    Test fixes for change in lazy items loading

--- projects/gallery/src/lib/components/viewer/viewer.component.spec.ts
+++ projects/gallery/src/lib/components/viewer/viewer.component.spec.ts
@@ -116,6 +116,12 @@ describe('ViewerComponent Unit', () => {
         },
         {
           src: 'src2'
+        },
+        {
+          src: 'src3'
+        },
+        {
+          src: 'src4'
         }
       ];
       viewer.loading = 'lazy';
@@ -124,41 +130,33 @@ describe('ViewerComponent Unit', () => {
 
     it('should be truthy if default loading on', () => {
       viewer.loading = 'auto';
-      expect(viewer.getSrc(viewer.items[1])).toBeTruthy();
+      expect(viewer.getSrc(viewer.items[2])).toBeTruthy();
     });
 
-    it('should be empty if lazy loading on and item has not been seen nor selected yet', () => {
-      expect(viewer.getSrc(viewer.items[1])).toBeFalsy();
-    });
-
-    it(`should be truthy if item:
-          +- 1 item distance from selected item
-          lazy loaded
-          not yet seen
-          viewer was already interacted with`, () => {
-      viewer.onInteraction();
-      expect(viewer.getSrc(viewer.items[1])).toBeTruthy();
-    });
-
-    it(`should be falsy if item:
-          +- 1 item distance from selected item
-          lazy loaded
-          not yet seen
-          viewer wasn't interacted with yet
-          viewer has looping on`, () => {
-      viewer.loop = true;
-      viewer.items.push({ src: 'src3' });
+    it('should be falsy if lazy loading on and item has not been seen nor selected yet', () => {
       expect(viewer.getSrc(viewer.items[2])).toBeFalsy();
     });
 
     it('should be truthy if lazy loading on and item has been seen', () => {
-      viewer.items[1]._seen = true;
-      expect(viewer.getSrc(viewer.items[1])).toBeTruthy();
+      viewer.items[2]._seen = true;
+      expect(viewer.getSrc(viewer.items[2])).toBeTruthy();
     });
 
     it('should be truthy if lazy loading on and item has been selected', () => {
+      viewer.selectedIndex = 2;
+      expect(viewer.getSrc(viewer.items[2])).toBeTruthy();
+    });
+
+    it('should load items around selected item even though lazy loading is on', () => {
       viewer.selectedIndex = 1;
+      expect(viewer.getSrc(viewer.items[0])).toBeTruthy();
+      expect(viewer.getSrc(viewer.items[2])).toBeTruthy();
+    });
+
+    it('should load items around selected item even though lazy loading is on, with overlap to the end of list if looping is on', () => {
+      viewer.loop = true;
       expect(viewer.getSrc(viewer.items[1])).toBeTruthy();
+      expect(viewer.getSrc(viewer.items[3])).toBeTruthy();
     });
   });
 });
