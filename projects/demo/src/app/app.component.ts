55f9a516dae469c0ddc85fba8617e72d5e318485
dann <daniel.macak@email.cz>
2020-07-14 20:05:18 +0200

    Decreased size of images

--- projects/demo/src/app/app.component.ts
+++ projects/demo/src/app/app.component.ts
@@ -10,72 +10,83 @@ import { GalleryImage, GalleryVideo } from 'projects/gallery/src/lib/core';
 export class AppComponent {
   images = [
     new GalleryImage(
-      './assets/mountains-1-lg.jpg',
-      './assets/mountains-1-sm.jpg',
+      './assets/images/mountains-1-lg.jpg',
+      './assets/images/mountains-1-sm.jpg',
       'Mountains',
       'Mighty mountains'
     ),
     new GalleryImage(
-      './assets/house-1-lg.jpg',
-      './assets/house-1-sm.jpg',
+      './assets/images/house-1-lg.jpg',
+      './assets/images/house-1-sm.jpg',
       'House',
       `I just love mysterious houses`
     ),
     new GalleryImage(
-      './assets/church-1-lg.jpg',
-      './assets/church-1-sm.jpg',
+      './assets/images/church-1-lg.jpg',
+      './assets/images/church-1-sm.jpg',
       'Church hallway',
       'Feel the history in this beautiful church'
     ),
     new GalleryImage(
-      './assets/lens-1-lg.jpg',
-      './assets/lens-1-sm.jpg',
+      './assets/images/lens-1-lg.jpg',
+      './assets/images/lens-1-sm.jpg',
       'Lens',
       'Zoom the world'
     ),
     new GalleryImage(
-      './assets/tulip-1-lg.jpg',
-      './assets/tulip-1-sm.jpg',
+      './assets/images/mountains-2-md.jpg',
+      './assets/images/mountains-2-sm.jpg',
+      'Mountains',
+      'Sun lit mountains'
+    ),
+    new GalleryImage(
+      './assets/images/tulip-1-lg.jpg',
+      './assets/images/tulip-1-sm.jpg',
       'Tulip',
       `You didn't forget to buy your better half flowers, did you?`
     ),
   ];
 
+  smallerImages = this.images.map(i => ({
+    ...i,
+    src: i.src.replace('-lg', '-md'),
+  }));
+
   extendedImages = [
     ...this.images,
     new GalleryImage(
-      './assets/forest-1-lg.jpg',
-      './assets/forest-1-sm.jpg',
+      './assets/images/forest-1-lg.jpg',
+      './assets/images/forest-1-sm.jpg',
       'Forest',
       'Mysterious forest'
     ),
     new GalleryImage(
-      './assets/sky-1-lg.jpg',
-      './assets/sky-1-sm.jpg',
+      './assets/images/sky-1-lg.jpg',
+      './assets/images/sky-1-sm.jpg',
       'Sky',
       'Mysterious sky'
     ),
     new GalleryImage(
-      './assets/cheers-1-lg.jpg',
-      './assets/cheers-1-sm.jpg',
+      './assets/images/cheers-1-lg.jpg',
+      './assets/images/cheers-1-sm.jpg',
       'Cheers',
       'Two guys drinking during sunset'
     ),
     new GalleryImage(
-      './assets/laptop-1-lg.jpg',
-      './assets/laptop-1-sm.jpg',
+      './assets/images/laptop-1-lg.jpg',
+      './assets/images/laptop-1-sm.jpg',
       'Laptop',
       'Ideal workplace for computer work'
     ),
     new GalleryImage(
-      './assets/snowflake-1-lg.jpg',
-      './assets/snowflake-1-sm.jpg',
+      './assets/images/snowflake-1-lg.jpg',
+      './assets/images/snowflake-1-sm.jpg',
       'Snowflake',
       'Snowflake detail'
     ),
     new GalleryImage(
-      './assets/mesh-1-lg.jpg',
-      './assets/mesh-1-sm.jpg',
+      './assets/images/mesh-1-lg.jpg',
+      './assets/images/mesh-1-sm.jpg',
       'City',
       'City at night'
     ),
@@ -83,12 +94,12 @@ export class AppComponent {
 
   erroredImages = [
     new GalleryImage(
-      './assets/non-existing-picture.jpg',
+      './assets/images/non-existing-picture.jpg',
       null,
       'Non-existing picture'
     ),
     new GalleryVideo(
-      './assets/non-existing-video.jpg',
+      './assets/images/non-existing-video.jpg',
       null,
       'Non-existing video'
     ),
@@ -97,7 +108,7 @@ export class AppComponent {
 
   videos = [
     new GalleryVideo('https://www.youtube.com/embed/80_39eAx3z8'),
-    new GalleryVideo('./assets/beach-1.mp4'),
+    new GalleryVideo('./assets/images/beach-1.mp4'),
   ];
 
   mobile = matchMedia('(max-width: 767px)').matches;
--- projects/demo/src/app/components/demo-custom-templates/demo-custom-templates.component.ts
+++ projects/demo/src/app/components/demo-custom-templates/demo-custom-templates.component.ts
@@ -10,44 +10,44 @@ import { GalleryImage, GalleryVideo } from 'projects/gallery/src/lib/core';
 export class DemoCustomTemplatesComponent {
   images = [
     new GalleryImage(
-      './assets/mountains-1-lg.jpg',
-      './assets/mountains-1-sm.jpg',
+      './assets/images/mountains-1-lg.jpg',
+      './assets/images/mountains-1-sm.jpg',
       'Mountains',
       'Mighty mountains'
     ),
     new GalleryVideo(
-      './assets/beach-1.mp4',
+      './assets/images/beach-1.mp4',
       null,
       'Beach',
       'Video of a beach taken from drone'
     ),
     new GalleryImage(
-      './assets/non-existing-image.jpg',
+      './assets/images/non-existing-image.jpg',
       null,
       'Sky',
       'Mysterious sky'
     ),
     new GalleryImage(
-      './assets/cheers-1-lg.jpg',
-      './assets/cheers-1-sm.jpg',
+      './assets/images/cheers-1-lg.jpg',
+      './assets/images/cheers-1-sm.jpg',
       'Cheers',
       'Two guys drinking during sunset'
     ),
     new GalleryImage(
-      './assets/laptop-1-lg.jpg',
+      './assets/images/laptop-1-lg.jpg',
       null,
       'Laptop',
       'Ideal workplace for computer work'
     ),
     new GalleryImage(
-      './assets/snowflake-1-lg.jpg',
-      './assets/snowflake-1-sm.jpg',
+      './assets/images/snowflake-1-lg.jpg',
+      './assets/images/snowflake-1-sm.jpg',
       'Snowflake',
       'Snowflake detail'
     ),
     new GalleryImage(
-      './assets/mesh-1-lg.jpg',
-      './assets/mesh-1-sm.jpg',
+      './assets/images/mesh-1-lg.jpg',
+      './assets/images/mesh-1-sm.jpg',
       'City',
       'City at night'
     ),
