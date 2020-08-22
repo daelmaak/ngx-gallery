66dd7f558514d64c3bbea6aef963df25ad218a61
Daniel Macak <daniel.macak@email.cz>
2020-08-22 19:04:17 +0200

    Fixed leaking MediaDirective

--- projects/gallery/src/lib/core/index.ts
+++ projects/gallery/src/lib/core/index.ts
@@ -3,10 +3,11 @@ export {
   GalleryItem,
   GalleryImage,
   GalleryVideo,
-  GalleryItemEvent
+  GalleryItemEvent,
 } from './gallery-item';
 export * from './object-fit';
 export * from './loading';
 export * from './orientation';
 export * from './template-contexts';
 export { Aria } from './aria';
+export { MediaDirective } from '../directives/media.directive';
