19765a237d5847288859508a63eba15080365595
Daniel Macak <daniel.macak@post.at>
2020-12-04 19:28:12 +0100

    Added intersectionobserver polyfill to demo

--- projects/demo/src/polyfills.ts
+++ projects/demo/src/polyfills.ts
@@ -55,8 +55,8 @@
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
-import 'zone.js/dist/zone';  // Included with Angular CLI.
-
+import 'intersection-observer';
+import 'zone.js/dist/zone'; // Included with Angular CLI.
 
 /***************************************************************************************************
  * APPLICATION IMPORTS
