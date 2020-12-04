a2a52064a266f6b25f67ee9c09edc3f56164b36e
Daniel Macak <daniel.macak@post.at>
2020-12-04 19:38:34 +0100

    Prevented errors due to customEvent ctor in ie11

--- projects/gallery/src/lib/core/support.ts
+++ projects/gallery/src/lib/core/support.ts
@@ -3,9 +3,10 @@ export const isBrowser = typeof window !== 'undefined';
 export const SUPPORT = {
   scrollBehavior: isBrowser && 'scrollBehavior' in document.body.style,
   nativeMediaLoading: isBrowser && 'loading' in Image.prototype,
-  intersectionObserver: isBrowser && 'IntersectionObserver' in window
+  intersectionObserver: isBrowser && 'IntersectionObserver' in window,
 };
 
 export const UA = {
-  ios: isBrowser && !!window.navigator.userAgent.match(/iP(ad|hone|od)/)
+  ios: isBrowser && !!window.navigator.userAgent.match(/iP(ad|hone|od)/),
+  ie11: !!window['MSInputMethodContext'] && !!document['documentMode'],
 };
