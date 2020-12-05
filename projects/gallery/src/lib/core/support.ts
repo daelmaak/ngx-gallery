4fc3bba7dac0fb2fadb13013ead81294ec918f8f
Daniel Macak <daniel.macak@post.at>
2020-12-05 08:39:12 +0100

    Removed unused support properties

--- projects/gallery/src/lib/core/support.ts
+++ projects/gallery/src/lib/core/support.ts
@@ -2,8 +2,6 @@ export const isBrowser = typeof window !== 'undefined';
 
 export const SUPPORT = {
   scrollBehavior: isBrowser && 'scrollBehavior' in document.body.style,
-  nativeMediaLoading: isBrowser && 'loading' in Image.prototype,
-  intersectionObserver: isBrowser && 'IntersectionObserver' in window,
 };
 
 export const UA = {
