867754206ae446bcd8286d14300f83194605a161
Daniel Macak <daelmaak@gmail.com>
2021-12-23 15:50:18 +0100

    remove IE11 code

--- projects/gallery/src/lib/core/support.ts
+++ projects/gallery/src/lib/core/support.ts
@@ -5,6 +5,5 @@ export const SUPPORT = {
 };
 
 export const UA = {
-  ios: isBrowser && !!window.navigator.userAgent.match(/iP(ad|hone|od)/),
-  ie11: isBrowser && !!window['MSInputMethodContext'] && !!document['documentMode'],
+  ios: isBrowser && !!window.navigator.userAgent.match(/iP(ad|hone|od)/)
 };
