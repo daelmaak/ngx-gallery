2e1dc759f1269143da77dd8c7f29ecfe9cb8f69c
Daniel Macak <daniel.macak@email.cz>
2023-07-30 14:08:26 +0200

    feat: do not require media directive for custom views
    Previously, when using custom viewer template, one had to attach media
    directive to the img, video etc. so that the gallery registered whether
    assets got loaded or errored. This is easy to miss and frustrating to
    deal with, so I transformed the directive to an internal thing.
    
    Now it unifies the loading by capturing the load/error events in capture
    phase regardless of the assets being internal or external.

--- projects/gallery/src/lib/directives/media.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { MediaDirective } from './media.directive';
-
-describe('MediaDirective', () => {
-  it('should create an instance', () => {
-    const directive = new MediaDirective(null);
-    expect(directive).toBeTruthy();
-  });
-});
