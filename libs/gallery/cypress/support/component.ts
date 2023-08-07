3b5cb1672119f1b27448434924154c8b968ab2cc
Daniel Macak <damacak@cisco.com>
2023-08-07 22:56:42 +0200

    test(gallery): introduce cypress component testing
    The plan is to use cypress for UI heavy testing, like checking the
    position of the slider and such. I think this is the best option to test
    as close to user's experience as possible.
    
    The previous solution with Karma and headless Chrome just doesn't suffice
    anymore - it is not easy to debug nor to simulate user's behavior. For
    instance, to simulate user sliding, I have to manually dispatch events
    on DOM nodes. Or I have to manually flush async tasks. That is not ideal
    for maintenance, to put it mildly.
    
    One note regarding webpack: For some reason, incorrect webpack types were
    installed and there was a mismatch between expected and actual types.
    Installing them explicitly solved the problem.

---
+++ libs/gallery/cypress/support/component.ts
@@ -0,0 +1,29 @@
+import { mount } from 'cypress/angular';
+// ***********************************************************
+// This example support/component.ts is processed and
+// loaded automatically before your test files.
+//
+// This is a great place to put global configuration and
+// behavior that modifies Cypress.
+//
+// You can change the location of this file or turn off
+// automatically serving support files with the
+// 'supportFile' configuration option.
+//
+// You can read more here:
+// https://on.cypress.io/configuration
+// ***********************************************************
+
+// Import commands.ts using ES2015 syntax:
+import './commands';
+
+// add component testing only related command here, such as mount
+declare global {
+  namespace Cypress {
+    interface Chainable<Subject> {
+      mount: typeof mount;
+    }
+  }
+}
+
+Cypress.Commands.add('mount', mount);
