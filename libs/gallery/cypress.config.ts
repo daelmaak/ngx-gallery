a360da37314681844e265945ac22e8a0b6ab6be9
Daniel Macak <daniel.macak@email.cz>
2024-02-21 22:52:29 +0100

    test(gallery): improve testing config
    First, I preselect Electron as the running browser, which skips the
    manual browser selection step locally.
    
    Then I set the config for command timeout to 1s instead of 4s, because
    it makes the testing flow faster and there is no reason to wait that
    long.
    
    Lastly I set the waitForAnimations to false, but to me it doesn't seem
    like it has an effect. It's purpose is to either wait or not for an
    element to finish animating before running commands.

--- libs/gallery/cypress.config.ts
+++ libs/gallery/cypress.config.ts
@@ -3,6 +3,8 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   component: nxComponentTestingPreset(__filename),
+  defaultCommandTimeout: 1000,
   viewportWidth: 600,
   viewportHeight: 700,
+  waitForAnimations: false,
 });
