cba5d823b2615951659a38168f705b2b4531c48f
Daniel Macak <damacak@cisco.com>
2026-03-21 18:51:18 +0100

    build(gallery): fix linting
    My new tsconfig project references require a projectService instead of
    project setting.

--- libs/gallery/eslint.config.js
+++ libs/gallery/eslint.config.js
@@ -11,7 +11,7 @@ export default defineConfig([
 
     languageOptions: {
       parserOptions: {
-        project: ['libs/gallery/tsconfig.*?.json'],
+        projectService: ['libs/gallery/tsconfig.json'],
       },
     },
 
